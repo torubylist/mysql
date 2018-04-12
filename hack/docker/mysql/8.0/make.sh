46065e18261c485f05225ea88b4079451484cd39
Maruf Tuhin <maruf.2hin@gmail.com>
2018-04-12 10:14:24 +0600

    Use mysql:8.0.3 image as mysql:8.0 (#26)

--- hack/docker/mysql/8.0/make.sh
+++ hack/docker/mysql/8.0/make.sh
@@ -5,11 +5,12 @@ DOCKER_REGISTRY=${DOCKER_REGISTRY:-kubedb}
 IMG=mysql
 TAG=8.0
 ALT_TAG=8
+PATCH=8.0.3
 
-docker pull $IMG:$TAG
+docker pull $IMG:$PATCH
 
-docker tag $IMG:$TAG "$DOCKER_REGISTRY/$IMG:$TAG"
+docker tag $IMG:$PATCH "$DOCKER_REGISTRY/$IMG:$TAG"
 docker push "$DOCKER_REGISTRY/$IMG:$TAG"
 
-docker tag $IMG:$TAG "$DOCKER_REGISTRY/$IMG:$ALT_TAG"
+docker tag $IMG:$PATCH "$DOCKER_REGISTRY/$IMG:$ALT_TAG"
 docker push "$DOCKER_REGISTRY/$IMG:$ALT_TAG"
