b1890f7ccb1ccbbf834b83239d15d856affa7c7f
Tamal Saha <tamal@appscode.com>
2018-05-03 13:30:44 -0700

    Update client-go to 7.0.0 (#33)

--- pkg/cmds/server/start.go
+++ pkg/cmds/server/start.go
@@ -56,7 +56,7 @@ func (o MySQLServerOptions) Config() (*server.MySQLServerConfig, error) {
 	}
 
 	serverConfig := genericapiserver.NewRecommendedConfig(server.Codecs)
-	if err := o.RecommendedOptions.ApplyTo(serverConfig); err != nil {
+	if err := o.RecommendedOptions.ApplyTo(serverConfig, server.Scheme); err != nil {
 		return nil, err
 	}
 	serverConfig.EnableMetrics = true
