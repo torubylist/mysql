c519ef89a01bbe17d1f08e4098c04917fe2d23f6
Tamal Saha <tamal@appscode.com>
2018-07-08 12:42:45 -0700

    Format shell script (#51)
    find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -8,18 +8,18 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker run hello-world
 
 # install python pip
-apt-get update > /dev/null
-apt-get install -y python python-pip > /dev/null
+apt-get update >/dev/null
+apt-get install -y python python-pip >/dev/null
 
 # install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &>/dev/null
 chmod +x ./kubectl
 mv ./kubectl /bin/kubectl
 
 # install onessl
-curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
-  && chmod +x onessl \
-  && mv onessl /usr/local/bin/
+curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 &&
+  chmod +x onessl &&
+  mv onessl /usr/local/bin/
 
 # install pharmer
 pushd /tmp
@@ -28,19 +28,19 @@ chmod +x pharmer-linux-amd64
 mv pharmer-linux-amd64 /bin/pharmer
 popd
 
-function cleanup {
-    # delete cluster on exit
-    pharmer get cluster || true
-    pharmer delete cluster $NAME || true
-    pharmer get cluster || true
-    sleep 120 || true
-    pharmer apply $NAME || true
-    pharmer get cluster || true
+function cleanup() {
+  # delete cluster on exit
+  pharmer get cluster || true
+  pharmer delete cluster $NAME || true
+  pharmer get cluster || true
+  sleep 120 || true
+  pharmer apply $NAME || true
+  pharmer get cluster || true
 
-    # delete docker image on exit
-    curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
-    chmod +x docker.py || true
-    ./docker.py del_tag kubedbci my-operator $CUSTOM_OPERATOR_TAG
+  # delete docker image on exit
+  curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
+  chmod +x docker.py || true
+  ./docker.py del_tag kubedbci my-operator $CUSTOM_OPERATOR_TAG
 }
 trap cleanup EXIT
 
@@ -62,7 +62,7 @@ export DOCKER_REGISTRY=kubedbci
 popd
 
 # create credential file for pharmer
-cat > cred.json <<EOF
+cat >cred.json <<EOF
 {
         "token" : "$TOKEN"
 }
@@ -80,7 +80,7 @@ sleep 120
 kubectl get nodes
 
 # create storageclass
-cat > sc.yaml <<EOF
+cat >sc.yaml <<EOF
 apiVersion: storage.k8s.io/v1
 kind: StorageClass
 metadata:
@@ -99,7 +99,7 @@ export CRED_DIR=$(pwd)/creds/gcs/gcs.json
 pushd $GOPATH/src/github.com/kubedb/mysql
 
 # create config/.env file that have all necessary creds
-cat > hack/config/.env <<EOF
+cat >hack/config/.env <<EOF
 AWS_ACCESS_KEY_ID=$AWS_KEY_ID
 AWS_SECRET_ACCESS_KEY=$AWS_SECRET
 
@@ -122,10 +122,9 @@ AZURE_CONTAINER_NAME=$AZURE_CONTAINER_NAME
 SWIFT_CONTAINER_NAME=$SWIFT_CONTAINER_NAME
 EOF
 
-
 # run tests
 source ./hack/deploy/setup.sh --docker-registry=kubedbci
-./hack/make.py test e2e --v=1 --storageclass=standard --selfhosted-operator=true  2> /dev/null
+./hack/make.py test e2e --v=1 --storageclass=standard --selfhosted-operator=true 2>/dev/null
 
 # Print Workload Descriptions if the test fails
 if [ $? -ne 0 ]; then
--- hack/docker/mysql-tools/8.0/make.sh
+++ hack/docker/mysql-tools/8.0/make.sh
@@ -17,27 +17,27 @@ DIST=$REPO_ROOT/dist
 mkdir -p $DIST
 
 build() {
-    pushd "$REPO_ROOT/hack/docker/mysql-tools/$TAG"
+  pushd "$REPO_ROOT/hack/docker/mysql-tools/$TAG"
 
-    # Download osm
-    wget https://cdn.appscode.com/binaries/osm/${OSM_VER}/osm-alpine-amd64
-    chmod +x osm-alpine-amd64
-    mv osm-alpine-amd64 osm
+  # Download osm
+  wget https://cdn.appscode.com/binaries/osm/${OSM_VER}/osm-alpine-amd64
+  chmod +x osm-alpine-amd64
+  mv osm-alpine-amd64 osm
 
-    local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
-    echo $cmd; $cmd
+  local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  echo $cmd; $cmd
 
-    rm osm
-    popd
+  rm osm
+  popd
 }
 
 docker_push() {
-    local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG $DOCKER_REGISTRY/$IMG:$ALT_TAG"
-	echo $cmd; $cmd
-	cmd="docker push $DOCKER_REGISTRY/$IMG:$ALT_TAG"
-	echo $cmd; $cmd
+  local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG $DOCKER_REGISTRY/$IMG:$ALT_TAG"
+  echo $cmd; $cmd
+  cmd="docker push $DOCKER_REGISTRY/$IMG:$ALT_TAG"
+  echo $cmd; $cmd
 
-	hub_canary
+  hub_canary
 }
 
 binary_repo $@
