fb28bc6cff6a0f2f656022434fcf78f54e201853
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-07-31 07:19:48 +0600

    Refactor concourse scripts (#56)

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -1,140 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -x -e
+set -eoux pipefail
 
-# start docker and log-in to docker-hub
-entrypoint.sh
-docker login --username=$DOCKER_USER --password=$DOCKER_PASS
-docker run hello-world
+REPO_NAME=mysql
+OPERATOR_NAME=my-operator
 
-# install python pip
-apt-get update >/dev/null
-apt-get install -y python python-pip >/dev/null
-
-# install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &>/dev/null
-chmod +x ./kubectl
-mv ./kubectl /bin/kubectl
-
-# install onessl
-curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 &&
-  chmod +x onessl &&
-  mv onessl /usr/local/bin/
-
-# install pharmer
-pushd /tmp
-curl -LO https://cdn.appscode.com/binaries/pharmer/0.1.0-rc.3/pharmer-linux-amd64
-chmod +x pharmer-linux-amd64
-mv pharmer-linux-amd64 /bin/pharmer
+# get concourse-common
+pushd $REPO_NAME
+git status
+git subtree pull --prefix hack/concourse/common https://github.com/kubedb/concourse-common.git master --squash -m 'concourse'
 popd
 
-function cleanup() {
-  # delete cluster on exit
-  pharmer get cluster || true
-  pharmer delete cluster $NAME || true
-  pharmer get cluster || true
-  sleep 120 || true
-  pharmer apply $NAME || true
-  pharmer get cluster || true
+source $REPO_NAME/hack/concourse/common/init.sh
 
-  # delete docker image on exit
-  curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
-  chmod +x docker.py || true
-  ./docker.py del_tag kubedbci my-operator $CUSTOM_OPERATOR_TAG
-}
-trap cleanup EXIT
+cp creds/gcs.json /gcs.json
+cp creds/.env $GOPATH/src/github.com/kubedb/$REPO_NAME/hack/config/.env
 
-# copy mysql to $GOPATH
-mkdir -p $GOPATH/src/github.com/kubedb
-cp -r mysql $GOPATH/src/github.com/kubedb
-
-pushd $GOPATH/src/github.com/kubedb/mysql
-# name of the cluster
-# nameing is based on repo+commit_hash
-NAME=mysql-$(git rev-parse --short HEAD)
+pushd "$GOPATH"/src/github.com/kubedb/$REPO_NAME
 
 ./hack/builddeps.sh
 export APPSCODE_ENV=dev
 export DOCKER_REGISTRY=kubedbci
-./hack/docker/my-operator/make.sh build
-./hack/docker/my-operator/make.sh push
-
-popd
-
-# create credential file for pharmer
-cat >cred.json <<EOF
-{
-        "token" : "$TOKEN"
-}
-EOF
-
-# create cluster using pharmer
-# note: make sure the zone supports volumes, not all regions support that
-# "We're sorry! Volumes are not available for Droplets on legacy hardware in the NYC3 region"
-pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
-pharmer create cluster $NAME --provider=digitalocean --zone=nyc1 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.10.0
-pharmer apply $NAME
-pharmer use cluster $NAME
-#wait for cluster to be ready
-sleep 120
-kubectl get nodes
-
-# create storageclass
-cat >sc.yaml <<EOF
-apiVersion: storage.k8s.io/v1
-kind: StorageClass
-metadata:
-  name: standard
-parameters:
-  zone: nyc1
-provisioner: external/pharmer
-EOF
-
-kubectl create -f sc.yaml
-sleep 60
-kubectl get storageclass
-
-export CRED_DIR=$(pwd)/creds/gcs/gcs.json
-
-pushd $GOPATH/src/github.com/kubedb/mysql
-
-# create config/.env file that have all necessary creds
-cat >hack/config/.env <<EOF
-AWS_ACCESS_KEY_ID=$AWS_KEY_ID
-AWS_SECRET_ACCESS_KEY=$AWS_SECRET
-
-GOOGLE_PROJECT_ID=$GCE_PROJECT_ID
-GOOGLE_APPLICATION_CREDENTIALS=$CRED_DIR
-
-AZURE_ACCOUNT_NAME=$AZURE_ACCOUNT_NAME
-AZURE_ACCOUNT_KEY=$AZURE_ACCOUNT_KEY
-
-OS_AUTH_URL=$OS_AUTH_URL
-OS_TENANT_ID=$OS_TENANT_ID
-OS_TENANT_NAME=$OS_TENANT_NAME
-OS_USERNAME=$OS_USERNAME
-OS_PASSWORD=$OS_PASSWORD
-OS_REGION_NAME=$OS_REGION_NAME
-
-S3_BUCKET_NAME=$S3_BUCKET_NAME
-GCS_BUCKET_NAME=$GCS_BUCKET_NAME
-AZURE_CONTAINER_NAME=$AZURE_CONTAINER_NAME
-SWIFT_CONTAINER_NAME=$SWIFT_CONTAINER_NAME
-EOF
+./hack/docker/$OPERATOR_NAME/make.sh build
+./hack/docker/$OPERATOR_NAME/make.sh push
 
 # run tests
 source ./hack/deploy/setup.sh --docker-registry=kubedbci
-./hack/make.py test e2e --v=1 --storageclass=standard --selfhosted-operator=true 2>/dev/null
-
-# Print Workload Descriptions if the test fails
-if [ $? -ne 0 ]; then
-  echo ""
-  kubectl describe deploy -n kube-system -l app=kubedb
-  echo ""
-  echo ""
-  kubectl describe replicasets -n kube-system -l app=kubedb
-  echo ""
-  echo ""
-  kubectl describe pods -n kube-system -l app=kubedb
-  exit 1
-fi
+./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true --ginkgo.flakeAttempts=2
