326499c8787ffc0c3b069b400e317d22bffb3265
shudipta <shudipta@appscode.com>
2019-04-02 18:45:55 +0600

    cont.

--- hack/docker/mysql/5.7.25/entrypoint.sh
+++ hack/docker/mysql/5.7.25/entrypoint.sh
@@ -68,9 +68,9 @@ echo ""
 
 wait_for_each_instance_be_ready ${GROUP_SIZE} ${BASE_NAME} ${GOV_SVC} ${NAMESPACE}
 
-export ips=`get_ip_whitelist ${GROUP_SIZE} ${BASE_NAME} ${GOV_SVC} ${NAMESPACE}`
-echo ">>>>>> ips: $ips"
-export seeds=`echo -n ${ips} | sed -e "s/,/:33060,/g" && echo -n ":33060"`
+export hosts=`get_ip_whitelist ${GROUP_SIZE} ${BASE_NAME} ${GOV_SVC} ${NAMESPACE}`
+echo ">>>>>> ips: $hosts"
+export seeds=`echo -n ${hosts} | sed -e "s/,/:33060,/g" && echo -n ":33060"`
 echo ">>>>>> seeds: $seeds"
 declare -i srv_id=`hostname | sed -e "s/${BASE_NAME}-//g"`
 ((srv_id+=1))
@@ -84,7 +84,7 @@ echo ">>>>>> cur_addr: $cur_addr"
 
 #echo "hi" > tmp.cnf
 #cat >> tmp.cnf <<EOL
-##loose-group_replication_ip_whitelist = "${ips}"
+##loose-group_replication_ip_whitelist = "${hosts}"
 ##loose-group_replication_group_seeds = "$seeds"
 ##server_id = ${srv_id}
 ##bind-address = "${cur_host}"
@@ -92,6 +92,41 @@ echo ">>>>>> cur_addr: $cur_addr"
 ##loose-group_replication_local_address = "${cur_addr}"
 #EOL
 #cat tmp.cnf
+
+# -----------------
+## group replication pre-requisites & recommendations
+#binlog-format = ROW
+#gtid-mode = ON
+#enforce-gtid-consistency = ON
+#log-slave-updates = ON
+#master-info-repository = TABLE
+#relay-log-info-repository = TABLE
+#binlog-checksum = NONE
+#slave-preserve-commit-order = ON
+## prevent use of non-transactional storage engines
+#disabled_storage_engines="MyISAM,BLACKHOLE,FEDERATED,ARCHIVE"
+## InnoDB gap locks are problematic for multi-primary conflict detection; none are used with READ-COMMITTED
+## So if you don't rely on REPEATABLE-READ semantics and/or wish to use multi-primary mode then this
+## isolation level is recommended
+#transaction-isolation = 'READ-COMMITTED'
+## Let Group Replication manage the READ/WRITE mode of the server entirely throughout the process lifecycle
+## super_read_only = ON
+#
+## group replication specific options
+#transaction-write-set-extraction = XXHASH64
+## disable the IP whitelisting, as we won't know what the IP range will be
+#loose-group-replication-ip-whitelist='0.0.0.0/0'
+#
+## dynamic options to be driven by command-line parameters
+##****************************************************************************
+## server-id = ??? # must be unique within replication topologies
+## group_replication_group_name = ??? # must be valid UUID
+## group_replication_bootstrap_group = ??? # must be ON for the first node in the group
+## group_replication_local_address = ??? # needs to be an IP address / hostname which the other GR containers can reach
+## group_replication_group_seeds = ??? # is necessary for the non-bootstrap nodes
+##****************************************************************************
+# -----------------
+
 
 echo "/etc/mysql/my.cnf contents are as follows:"
 cat >> /etc/mysql/my.cnf <<EOL
@@ -115,7 +150,7 @@ loose-group_replication_recovery_use_ssl = 1
 
 # Shared replication group configuration
 loose-group_replication_group_name = "${GROUP_NAME}"
-loose-group_replication_ip_whitelist = "${ips}"
+loose-group_replication_ip_whitelist = "${hosts}"
 loose-group_replication_group_seeds = "${seeds}"
 
 # Single or Multi-primary mode? Uncomment these two lines
@@ -135,58 +170,94 @@ echo ""
 cat /etc/mysql/my.cnf
 echo ""
 
-#while true; do
-#    echo .
-#    sleep 1
-#done
-
-echo "$(timestamp) [INFO] Starting mysql server..."
-docker-entrypoint.sh mysqld &>/dev/null &
-echo "$(timestamp) [INFO] Waiting for the server being run..."
-#sleep 30
 while true; do
-    echo ">>>>>>>>>> pining..."
-    out=`mysqladmin -u root --password=uWuj7-dbvefZVnJx ping 2> /dev/null || true`
-    echo ">>>>>>>> out:$out"
-    if [[ "$out" == "mysqld is alive" ]]; then
-        sleep 5
-        break
-    fi
+#    echo .
     sleep 1
 done
 
+#echo "$(timestamp) [INFO] Starting mysql server..."
+#docker-entrypoint.sh mysqld >/dev/null 2>&1 &
+#pid=$!
+#echo "$(timestamp) [INFO] Waiting for the server being run..."
+##sleep 30
 #while true; do
-#    echo .
+#    echo ">>>>>>>>>> pining..."
+#    out=`mysqladmin -u root --password=uWuj7-dbvefZVnJx ping 2> /dev/null || true`
+#    echo ">>>>>>>> out:$out"
+#    if [[ "$out" == "mysqld is alive" ]]; then
+#        sleep 5
+#        break
+#    fi
 #    sleep 1
 #done
+#
+##while true; do
+##    echo .
+##    sleep 1
+##done
+#
+#echo "$(timestamp) [INFO] Initialing the server..."
+#export mysql_header="mysql -u root --password=uWuj7-dbvefZVnJx"
+#out=`${mysql_header} -N -e "select count(host) from mysql.user where mysql.user.user='repl';" 2> /dev/null | awk '{print$1}'`
+#if [[ "$out" -eq "0" ]]; then
+#    ${mysql_header} -N -e "SET SQL_LOG_BIN=0;" 2> /dev/null
+#    ${mysql_header} -N -e "CREATE USER 'repl'@'%' IDENTIFIED BY 'password' REQUIRE SSL;" 2> /dev/null
+#    ${mysql_header} -N -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';" 2> /dev/null
+#    ${mysql_header} -N -e "FLUSH PRIVILEGES;" 2> /dev/null
+#    ${mysql_header} -N -e "SET SQL_LOG_BIN=1;" 2> /dev/null
+#
+#    echo ">>>> plugins are as follows:"
+#    out=`${mysql_header} -N -e "SHOW PLUGINS;" 2> /dev/null || true`
+#    echo ">>>> plugins= $out"
+#fi
+##mysql -u root --password=uWuj7-dbvefZVnJx
+### mysql>
+##SET SQL_LOG_BIN=0;
+##CREATE USER 'repl'@'%' IDENTIFIED BY 'password' REQUIRE SSL;
+##GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
+##FLUSH PRIVILEGES;
+### -> from here
+##SET SQL_LOG_BIN=1;
+#
+#${mysql_header} -N -e "CHANGE MASTER TO MASTER_USER='repl', MASTER_PASSWORD='password' FOR CHANNEL 'group_replication_recovery';" 2> /dev/null
+#out=`${mysql_header} -N -e 'SHOW PLUGINS;' 2> /dev/null | grep group_replication`
+#if [[ -z "$out" ]]; then
+#    ${mysql_header} -e "INSTALL PLUGIN group_replication SONAME 'group_replication.so';" 2> /dev/null
+#fi
+#
+## TODO: it is optional. So remove this
+#echo ">>>> plugins are as follows:"
+#out=`${mysql_header} -N -e "SHOW PLUGINS;" 2> /dev/null || true`
+#echo ">>>> plugins= $out"
 
-echo "$(timestamp) [INFO] Initialing the server..."
-export mysql_header="mysql -u root --password=uWuj7-dbvefZVnJx"
-out=`${mysql_header} -N -e "select count(host) from mysql.user where mysql.user.user='repl';" 2> /dev/null | awk '{print$1}'`
-if [[ "$out" -eq "0" ]]; then
-    ${mysql_header} -N -e "SET SQL_LOG_BIN=0;" 2> /dev/null
-    ${mysql_header} -N -e "CREATE USER 'repl'@'%' IDENTIFIED BY 'password' REQUIRE SSL;" 2> /dev/null
-    ${mysql_header} -N -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';" 2> /dev/null
-    ${mysql_header} -N -e "FLUSH PRIVILEGES;" 2> /dev/null
-    ${mysql_header} -N -e "SET SQL_LOG_BIN=1;" 2> /dev/null
+function check() {
+    local gr_size=$1
+    local base_name=$2
+    local gov_svc=$3
+    local ns=$4
 
-    out=`${mysql_header} -e "SHOW PLUGINS;" 2> /dev/null || true`
-    echo ">>>> plugins= $out"
-fi
-#mysql -u root --password=uWuj7-dbvefZVnJx
-## mysql>
-#SET SQL_LOG_BIN=0;
-#CREATE USER 'repl'@'%' IDENTIFIED BY 'password' REQUIRE SSL;
-#GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
-#FLUSH PRIVILEGES;
-## -> from here
-#SET SQL_LOG_BIN=1;
+    # TODO: Need to handle for multiple group existence
+    local member_hosts=( `echo -n ${hosts} | sed -e "s/,/ /g"` )
 
-${mysql_header} -N -e "CHANGE MASTER TO MASTER_USER='repl', MASTER_PASSWORD='password' FOR CHANNEL 'group_replication_recovery';" 2> /dev/null
-out=`(${mysql_header} -e 'SHOW PLUGINS;' 2> /dev/null || true)| grep group_replication`
-if [[ "$out" -eq "" ]]; then
-    ${mysql_header} -e "INSTALL PLUGIN group_replication SONAME 'group_replication.so';" 2> /dev/null
-fi
-# TODO: it is optional. So remove thisex
-out=`${mysql_header} -e "SHOW PLUGINS;" 2> /dev/null || true`
-echo ">>>> plugins= $out"
+    group_found=0
+    for host in ${member_hosts[@]}; do
+
+        export mysql_header="mysql -u root --password=uWuj7-dbvefZVnJx --host=${host}"
+        primary_id=${mysql_header} -N -e "SHOW STATUS LIKE '%primary%';" 2>/dev/null
+        ${mysql_header} -N -e "SELECT MEMBER_PORT FROM performance_schema.replication_group_members;"
+        ids=( `${mysql_header} -N -e "SELECT MEMBER_ID FROM performance_schema.replication_group_members WHERE MEMBER_STATE = 'ONLINE' OR MEMBER_STATE = 'RECOVERING';" 2>/dev/null` )
+
+        for id in ${ids[@]}; do
+            if [[ -n "${primary_id}" -a "${primary_id}" == "${id}" ]]; then
+                group_found=1
+            fi
+        done
+    done
+
+    for ((i=0;i<$gr_size;i++)); do
+        if (("$i" > "0")); then
+            echo -n ",";
+        fi
+        echo -n "$base_name-$i.$gov_svc.$ns.svc"
+    done
+}
